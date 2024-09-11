db6121ea0a4dffd4199841a8d767c1b04d8ab31e
CadenceRiddle <cadence475@gmail.com>
2024-09-11 13:05:58 -0400

    deleted testing file

--- tests/main.test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from src import hello
-
-class TestSample(unittest.TestCase):
-    def test_function(self):
-        self.assertEqual(hello(), 'hello world')
-
-if __name__ == '__main__':
-    unittest.main()
